764fe49f6bdd8d20262e307d84034eabd86b4557
ChrisPChip <11971429@student.uts.edu.au>
2020-10-10 21:00:54 +1100

    Working Environment class and main that works with said class

--- Environment.m
+++ Environment.m
@@ -2,8 +2,19 @@ classdef Environment < handle
     
     properties
         
-        % Environment area
-%         workspace = [-3 3 -3 3 -0.5 2];
+        %% Object Transforms
+        % Environment Objects
+        table = [0 3 0];
+        floor = [0 0 0];
+        wall = [0 5 1.5];
+        mirror = [-2 2.5 1.5];
+        shelf = [0 .5 0];
+        ext = [-1.6 0 0];
+        
+        % Cans
+        red = [-0.5 3 1.08];
+        green = [-1 3 1.08];
+        blue = [1 3 1.08];
         
         %% 3D model parameters
         % Ply data
@@ -11,96 +22,87 @@ classdef Environment < handle
         v;
         data;
         
-        %         tableMesh_h;
-        %         floorMesh_h;
-        %         wallMesh_h;
-        %         mirrorMesh_h;
-        %         blueCanMesh_h;
-        %         greenCanMesh_h;
-        %         redCanMesh_h;
-        %         shelfMesh_h;
-        %         extinguisherMesh_h;
-        
     end
     
     %% Public Methods
     methods (Static)
         
         function generateObjects(obj)
-            obj.generateEnvironment();
-            obj.generateCans();
+            obj.generateEnvironment(obj);
+            obj.generateCans(obj);
         end
     end
     
     %% Private Methods
     methods (Static)
         
-        function generateEnvironment()
+        function generateEnvironment(obj)
+            %% Insert PLY data
             % Table
             [f,v,data] = plyread('table.ply','tri');
             vertexColours = [data.vertex.red, data.vertex.green, data.vertex.blue] / 255;
-            tableMesh_h = trisurf(f,v(:,1),v(:,2)+3, v(:,3) ...
+            tableMesh_h = trisurf(f,v(:,1) + obj.table(1),v(:,2) + obj.table(2), v(:,3) + obj.table(3) ...
                 ,'FaceVertexCData',vertexColours,'EdgeColor','interp','EdgeLighting','flat');
             hold on
             
             % Floor
             [f,v,data] = plyread('Floor.ply','tri');
             vertexColours = [data.vertex.red, data.vertex.green, data.vertex.blue] / 255;
-            floorMesh_h = trisurf(f,v(:,1),v(:,2), v(:,3) ...
+            floorMesh_h = trisurf(f,v(:,1) + obj.floor(1),v(:,2) + obj.floor(2), v(:,3) + obj.floor(3) ...
                 ,'FaceVertexCData',vertexColours,'EdgeColor','interp','EdgeLighting','flat');
             hold on
             
             % Wall
             [f,v,data] = plyread('wall.ply','tri');
             vertexColours = [data.vertex.red, data.vertex.green, data.vertex.blue] / 255;
-            wallMesh_h = trisurf(f,v(:,1),v(:,2)+5, v(:,3)+1.5 ...
+            wallMesh_h = trisurf(f,v(:,1) + obj.wall(1),v(:,2) + obj.wall(2), v(:,3) +  obj.wall(3) ...
                 ,'FaceVertexCData',vertexColours,'EdgeColor','interp','EdgeLighting','flat');
             hold on
             
             % Mirror Wall
             [f,v,data] = plyread('mirrorwall.ply','tri');
             vertexColours = [data.vertex.red, data.vertex.green, data.vertex.blue] / 255;
-            mirrorMesh_h = trisurf(f,v(:,1)-2,v(:,2)+2.5, v(:,3)+1.5 ...
+            mirrorMesh_h = trisurf(f,v(:,1) + obj.mirror(1),v(:,2) + obj.mirror(2), v(:,3) + obj.mirror(3) ...
                 ,'FaceVertexCData',vertexColours,'EdgeColor','interp','EdgeLighting','flat');
             hold on
             
             % Shelf
             [f,v,data] = plyread('shelf.ply','tri');
             vertexColours = [data.vertex.red, data.vertex.green, data.vertex.blue] / 255;
-            shelfMesh_h = trisurf(f,v(:,1),v(:,2)+.5, v(:,3) ...
+            shelfMesh_h = trisurf(f,v(:,1) + obj.shelf(1),v(:,2) + obj.shelf(2), v(:,3) + obj.shelf(3) ...
                 ,'FaceVertexCData',vertexColours,'EdgeColor','interp','EdgeLighting','flat');
             hold on
             
             % Extinguisher
             [f,v,data] = plyread('extinguisher.ply','tri');
             vertexColours = [data.vertex.red, data.vertex.green, data.vertex.blue] / 255;
-            extinguisherMesh_h = trisurf(f,v(:,1)-1.6,v(:,2), v(:,3)...
+            extinguisherMesh_h = trisurf(f,v(:,1) + obj.ext(1),v(:,2) + obj.ext(2), v(:,3) + obj.ext(3)...
                 ,'FaceVertexCData',vertexColours,'EdgeColor','interp','EdgeLighting','flat');
             hold on
             
             
         end
         
-        function generateCans()
-            
+        function generateCans(obj)
+            %% Insert PLY Data
             % Red Can
             [f,v,data] = plyread('can.ply','tri');
             vertexColours = [data.vertex.red, data.vertex.green, data.vertex.blue] / 255;
-            redCanMesh_h = trisurf(f,v(:,1)-.5,v(:,2)+3, v(:,3)+1.08 ...
+            redCanMesh_h = trisurf(f,v(:,1) + obj.red(1),v(:,2) + obj.red(3), v(:,3) + obj.red(3) ...
                 ,'FaceVertexCData',vertexColours,'EdgeColor','interp','EdgeLighting','flat');
             hold on
             
             % Green Can
             [f,v,data] = plyread('greencan.ply','tri');
             vertexColours = [data.vertex.red, data.vertex.green, data.vertex.blue] / 255;
-            greenCanMesh_h = trisurf(f,v(:,1)-1,v(:,2)+3, v(:,3)+1.08 ...
+            greenCanMesh_h = trisurf(f,v(:,1) + obj.green(1),v(:,2) + obj.green(2), v(:,3) + obj.green(3) ...
                 ,'FaceVertexCData',vertexColours,'EdgeColor','interp','EdgeLighting','flat');
             hold on
             
             % Blue Can
             [f,v,data] = plyread('bluecan.ply','tri');
             vertexColours = [data.vertex.red, data.vertex.green, data.vertex.blue] / 255;
-            blueCanMesh_h = trisurf(f,v(:,1)+1,v(:,2)+3, v(:,3)+1.08 ...
+            blueCanMesh_h = trisurf(f,v(:,1) + obj.blue(1),v(:,2) + obj.blue(2), v(:,3) + obj.blue(3) ...
                 ,'FaceVertexCData',vertexColours,'EdgeColor','interp','EdgeLighting','flat');
             hold on
             
--- main.m
+++ main.m
@@ -9,6 +9,80 @@ environment.generateObjects(environment);
 hold on;
 robot = KinovaGen3(false);
 
-% while startPB == 1
-%     disp ('System is ok')
-% end
+RedCan = eye(4)*transl(environment.red(1),environment.red(2),environment.red(3));
+GreenCan = eye(4)*transl(environment.green(1),environment.green(2),environment.green(3));
+BlueCan = eye(4)*transl(environment.blue(1),environment.blue(2),environment.blue(3));
+str = input('Which colour should go on top? (red,grn,blu)','s');
+if str == 'red'
+    xr = 0; yr = 2; zr = 1.9;
+    xb = -.3; yb = 2; zb = 1;
+    xg = .3; yg = 2; zg = 1;
+elseif str == 'blu'
+    xr = -.3; yr = 2; zr = 1;
+    xb = 0; yb = 2; zb = 1.9;
+    xg = .3; yg = 2; zg = 1;
+elseif str == 'grn'
+    xr = -.3; yr = 2; zr = 1;
+    xb = .3; yb = 2; zb = 1;
+    xg = 0; yg = 2; zg = 1.9;
+else
+    disp ('Incorrect Input, placing all cans in middle shelf')
+    xr = -.3; yr = 2; zr = 1;
+    xb = 0; yb = 2; zb = 1;
+    xg = .3; yg = 2; zg = 1;
+end
+Finalred = makehgtform('translate',[xr,yr,zr]);
+Finalblue = makehgtform('translate',[xb,yb,zb]);
+Finalgreen = makehgtform('translate',[xg,yg,zg]);
+%% Animations
+q0 = [0 0 0 0 0 0 0];
+t1 = [0:0.5:8];
+q1 = robot.model.ikcon(RedCan,q0);
+Trajred = jtraj(q0,q1,t1);
+
+for i = 1:size(Trajred,1)
+    robot.model.animate(Trajred(i,:));
+    drawnow();
+end
+
+q2 = robot.model.ikcon(Finalred,q1);
+TrajFred = jtraj(q1,q2,t1);
+
+for i = 1:size(TrajFred,1)
+    robot.model.animate(TrajFred(i,:));
+    drawnow();
+end
+
+% Treajectory to blue
+q3 = robot.model.ikcon(q2);
+Trajblue = jtraj(q2,q3,t1);
+
+for i = 1:size(Trajblue,1)
+    robot.model.animate(Trajblue(i,:));
+    drawnow();
+end
+
+q4 = robot.model.ikcon(Finalblue,q3);
+TrajFblue = jtraj(q3,q4,t1);
+
+for i = 1:size(TrajFblue,1)
+    robot.model.animate(TrajFblue(i,:));
+    drawnow();
+end
+
+% Trajectory to green
+q5 = robot.model.ikcon(GreenCan,q4);
+Trajgreen = jtraj(q4,q5,t1);
+
+for i = 1:size(Trajgreen,1)
+    robot.model.animate(Trajgreen(i,:));
+    drawnow();
+end
+
+q6 = robot.model.ikcon(Finalgreen,q5);
+TrajFgreen = jtraj(q5,q6,t1);
+
+for i = 1:size(TrajFgreen,1)
+    robot.model.animate(TrajFgreen(i,:));
+    drawnow();
+end
